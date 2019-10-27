da0c3a16f60de86928d28839f6dd7d71d9e60a53
Mila Hose <mila.hose@gmail.com>
2019-10-26 22:54:15 -0700

    More fixes

--- tetris.js
+++ tetris.js
@@ -1,5 +1,10 @@
+'use strict';
+
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
+
+canvas.width = 240;
+canvas.height = 400;
 
 context.scale(20, 20);
 
@@ -23,37 +28,9 @@ const GAME = {
   ]
 }
 
-const arenaSweep = () => {
-  let rowCount = 1;
-  outer: for (let y = GAME.board.length -1; y > 0; y--) {
-    for (let x = 0; x < GAME.board[y].length; x++) {
-        if (GAME.board[y][x] === 0) {
-            continue outer;
-        }
-    }
-
-    const row = GAME.board.splice(y, 1)[0].fill(0);
-    GAME.board.unshift(row);
-    y--;
-
-    GAME.score += rowCount * 10;
-    rowCount *= 2;
-  }
-}
-
-const collide = board => {
-  const { tetromino, coords } = GAME;
-  for (let y = 0; y < tetromino.length; y++) {
-    for (let x = 0; x < tetromino[y].length; x++) {
-      if (tetromino[y][x] !== 0 && 
-         (board[y + coords.y] && 
-          board[y + coords.y][x + coords.x]) !== 0) {
-          return true;
-      }
-    }
-  }
-  return false;
-}
+const generateTetromino = () => (
+  tetrominoes['ILJOZST'[Math.floor(Math.random() * 7)]]
+)
 
 const createBoard = (width, height) => (
 	[...new Array(height)].map(() => [...new Array(width).fill(0)])
@@ -78,86 +55,99 @@ const draw = () => {
   drawBoard(GAME.tetromino, GAME.coords);
 }
 
-const merge = board => {
-    GAME.tetromino.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                board[y + GAME.coords.y][x + GAME.coords.x] = value;
-            }
-        });
-    });
-}
-
-const rotate = (tetromino, dir) => {
-  for (let y = 0; y < tetromino.length; ++y) {
-    for (let x = 0; x < y; ++x) {
-        [
-            tetromino[x][y],
-            tetromino[y][x],
-        ] = [
-            tetromino[y][x],
-            tetromino[x][y],
-        ];
-    }
-  }
-
-  if (dir > 0) {
-      tetromino.forEach(row => row.reverse());
-  } else {
-      tetromino.reverse();
-  }
-}
-
-const drop = () => {
-  GAME.coords.y++;
-  if (collide(GAME.board)) {
-      GAME.coords.y--;
-      merge(GAME.board);
-      resetGame();
-      arenaSweep();
-      updateScore();
-  }
-  GAME.dropCounter = 0;
-}
-
 const move = offset => {
   GAME.coords.x += offset;
-  if (collide(GAME.board)) {
+  if (collisionDetected()) {
     GAME.coords.x -= offset;
   }
 }
 
-const generateTetromino = () => (
-    tetrominoes['ILJOZST'[Math.floor(Math.random() * 7)]]
-)
+const rotate = () => {
+  const { tetromino } = GAME;
+  for (let y = 0; y < tetromino.length; ++y) {
+    for (let x = 0; x < y; ++x) {
+      [
+        tetromino[x][y],
+        tetromino[y][x],
+      ] = [
+        tetromino[y][x],
+        tetromino[x][y],
+      ];
+    }
+  }
+
+  tetromino.forEach(row => row.reverse());
+
+  while (collisionDetected()) {
+    GAME.coords.x += 1;
+  }
+}
+
+const merge = board => {
+  GAME.tetromino.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        board[y + GAME.coords.y][x + GAME.coords.x] = value;
+      }
+    });
+  });
+}
+
+const collisionDetected = () => {
+  const { board, tetromino, coords } = GAME;
+  for (let y = 0; y < tetromino.length; y++) {
+    for (let x = 0; x < tetromino[y].length; x++) {
+      if (tetromino[y][x] !== 0 &&
+        (board[y + coords.y] && board[y + coords.y][x + coords.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+const drop = () => {
+  GAME.coords.y++;
+  if (collisionDetected()) {
+    GAME.coords.y--;
+    merge(GAME.board);
+    resetGame();
+    tallyRows();
+    updateScore();
+  }
+  GAME.dropCounter = 0;
+}
+
+const tallyRows = () => {
+  let rowCount = 1;
+  outer: for (let y = GAME.board.length - 1; y > 0; --y) {
+    for (let x = 0; x < GAME.board[y].length; ++x) {
+      if (GAME.board[y][x] === 0) {
+        continue outer;
+      }
+    }
+
+    const row = GAME.board.splice(y, 1)[0].fill(0);
+    GAME.board.unshift(row);
+    ++y;
+
+    GAME.score += rowCount * 10;
+    rowCount *= 2;
+  }
+}
 
 const resetGame = () => {
   GAME.tetromino = generateTetromino();
   GAME.coords.y = 0;
   GAME.coords.x = 5;            
-  if (collide(GAME.board)) {
-      GAME.board.forEach(row => row.fill(0));
-      GAME.score = 0;
-      updateScore();
+  if (collisionDetected()) {
+    GAME.board.forEach(row => row.fill(0));
+    GAME.score = 0;
+    updateScore();
   }
 }
 
-const playerRotate = dir => {
-  const pos = GAME.coords.x;
-  let offset = 1;
-  rotate(GAME.tetromino, dir);
-  while (collide(arena)) {
-    GAME.coords.x += offset;
-    offset = -(offset + (offset > 0 ? 1 : -1));
-    if (offset > GAME.tetromino[0].length) {
-        rotate(GAME.tetromino, -dir);
-        GAME.coords.x = pos;
-        return;
-    }
-  }
-}
-
-const update = (time = 0) => {
+const startAnimation = (time = 0) => {
   const deltaTime = time - GAME.dropStart;
 
   GAME.dropCounter += deltaTime;
@@ -168,16 +158,16 @@ const update = (time = 0) => {
   GAME.dropStart = time;
 
   draw();
-  requestAnimationFrame(update);
+  requestAnimationFrame(startAnimation);
 }
 
 const updateScore = () => $('#score').text(GAME.score);
 
-const listen = () => {
+const readKeyInput = () => {
   $(document).on('keydown', ({code}) => {
     switch (code) {
       case 'ArrowUp':
-        playerRotate(1);
+        rotate();
         break;
       case 'ArrowDown':
         drop();
@@ -194,10 +184,10 @@ const listen = () => {
 
 const init = () => {
   GAME.board = createBoard(12, 20);
-  listen()
+  readKeyInput()
   resetGame();
   updateScore();
-  update();
+  startAnimation();
 }
 
 $(init);
